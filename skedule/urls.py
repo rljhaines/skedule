e5017dc0539ca1d25f6683d149c08aca5c09a078
rljhaines <rljhaines@gmail.com>
2015-01-23 17:22:43 -0600

    correct website url in skedule url.py

--- skedule/urls.py
+++ skedule/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'skedule.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^website/', include(website.views)),
 )
