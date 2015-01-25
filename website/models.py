e722b33b45839b0b80b2906bd47cbfc1420f0610
rljhaines <rljhaines@gmail.com>
2015-01-25 11:06:27 -0600

    found migration error

--- website/models.py
+++ website/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Greeting(models.Model):
     salutation = models.CharField(max_length=100)
     def __unicode__(self):
-        return self.name
+        return self.salutation
 
 # Create your models here.
