7ccb7add4779f78ce8827011ebe8aa9e6bb89280
rljhaines <rljhaines@gmail.com>
2015-01-25 10:09:38 -0600

    trying to get migration working

--- skedule/urls.py
+++ skedule/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from website import views 
+from website import views
 
 urlpatterns = patterns('',
     # Examples:
