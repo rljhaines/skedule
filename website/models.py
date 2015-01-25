0c01769cea5300a8742625873bc06d0925317543
rljhaines <rljhaines@gmail.com>
2015-01-25 10:13:40 -0600

    found a migtation error

--- website/models.py
+++ website/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Greeting(models.Model):
-    name = models.CharField(max_length=100)
+    salutation = models.CharField(max_length=100)
     def __unicode__(self):
         return self.name
 
